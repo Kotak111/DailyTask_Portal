0d3a981afea920be96e6f762290bd813ce81e966
Kotak111 <kotakh311@gmail.com>
2024-09-09 10:58:35 +0530

    dailytask

---
+++ client/src/component/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <>
+ 
+ <div className='home-main'>
+            <div className='welcome-home'>
+              <h2 className='wel-dev'>welcome to daily task submit portal</h2>
+              <a href="https://www.itechsarathi.in/" className='weare' target='_blank'>Itsarthi</a>
+            </div>
+          </div>
+   </>
+  )
+}
+
+export default Home
